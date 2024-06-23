ef331f2e5722e98910d09c4506987c45ef71a56b
sungeen Lim <cggoddog@naver.com>
2024-06-23 22:42:03 +0900

    forth commit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,21 +2,24 @@ import React from 'react'
 import {Container} from 'react-bootstrap';
 import { useNavigate, Link } from 'react-router-dom'
 
+
+
+
 const Navbar = () => {
-    const menuList = ['1. 상품 소개', '2. 채비야 상품보기', '3. 조합 요청', '4. 구매 요청']
+    // const menuList = ['1. 상품 소개', '2. 채비야 상품보기', '3. 조합 요청', '4. 구매 요청']
 
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
-    const goToPage = (event) => {
+    // const goToPage = (event) => {
 
-      console.log(event)
-    }
+    //   console.log(event)
+    // }
 
   return (
     <div>
       <Container>
         <div className="nav-area">
-         <img alt="" width={1300} src="https://cafeptthumb-phinf.pstatic.net/MjAyNDA2MDZfNyAg/MDAxNzE3NjUyOTI2NjQw.5Z0LXjCm3e0lfsbiIwdrtdhXDVyNCnakUauboXgaKYcg.c3FT5pKQ-Ag-BsYWUx3rpxRuauVeufUiIIi4ZulQ770g.JPEG/%ED%83%80%EC%9D%B4%ED%8B%80.jpg"/> 
+         <img src={process.env.PUBLIC_URL + 'img/chaebiya.jpg'} width={1300}/>
         </div>
         <div className="nav-area">
             <Link className="nav-menu" to={'/'}> 1. 상품 소개 </Link>
